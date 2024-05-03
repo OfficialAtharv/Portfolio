c97c7a0d87d3b9ef7b0f1630ba31b69b34f7122e
Atharv Kulkarni <152671663+OfficialAtharv@users.noreply.github.com>
2024-05-03 15:51:04 +0530

    Update About.jsx

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -7,9 +7,9 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-3">
+        <div className="pb-2">
           <p className="text-4xl pt-10px font-bold inline border-b-4 border-gray-500">
-            About me
+            About
           </p>
         </div>
         <p className="text-xl mt-20">
