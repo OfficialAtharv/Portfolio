20c55d6733a8b4bc7f1792ccbef83f7bf84b0def
Atharv Kulkarni <152671663+OfficialAtharv@users.noreply.github.com>
2024-05-03 15:59:22 +0530

    Update About.jsx

--- src/componants/About.jsx
+++ src/componants/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
         <div className="">
           <p className="text-4xl pt-10 font-bold inline border-b-4 border-gray-500">
-            About me
+            About 
           </p>
         </div>
         <p className="text-xl mt-20">
